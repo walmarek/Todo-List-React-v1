0fd4fdb36f620d2a88cc9af2c2e9e7588f551421
Marek Wal <walmarek1987@gmail.com>
2023-03-14 10:30:25 +0100

    source changed

--- src/features/tasks/getExampleTask.js
+++ src/features/tasks/getExampleTask.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-  const response = await fetch("/todo-list-react/exampleTasks.json");
+  const response = await fetch("/Todo-List-React-v1/exampleTasks.json");
 
   if (!response.ok) {
     new Error(response.statusText);
